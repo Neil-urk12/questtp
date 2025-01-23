d36eb56819c74b38b3d56ecadb6357b9ba51143d
Neil Vallecer <neilvallecer12@gmail.com>
2025-01-23 23:43:10 +0800

    fix: update import path for cmd package to reflect new directory structure

--- questtp/main.go
+++ questtp/main.go
@@ -1,10 +1,10 @@
 /*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+Copyright © 2025 NAME HERE <neilvallecer12@gmail.com>
 
 */
 package main
 
-import "questtp/cmd"
+import "questtp/questtp/cmd"
 
 func main() {
 	cmd.Execute()
